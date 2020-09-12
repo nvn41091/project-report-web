b4532519154004249a92905abff5c8b65c5c65a8
nvn41091 <nvn41091@gmail.com>
2020-09-12 18:51:26 +0700

    commit build

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+app.use(express.static('./dist/{{nvn41091}}'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname,'/dist/{{nvn41091}}/index.html'));
+});
+
+app.use(forceSSL());
+
+app.listen(process.env.PORT || 8080);
