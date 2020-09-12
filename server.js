ea69946a3fbe201039ac9c782d500450bbb79bdf
nvn41091 <nvn41091@gmail.com>
2020-09-12 21:01:26 +0700

    commit build

--- server.js
+++ server.js
@@ -3,22 +3,11 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const app = express();
 
-const forceSSL = function () {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-};
-app.use(express.static('./dist/{{nvn41091}}'));
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname,'/dist/{{nvn41091}}/index.html'));
-});
-
-app.use(forceSSL());
+app.use(express.static(__dirname + './dist'));
 
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname,'/dist/index.html'));
+});
+
