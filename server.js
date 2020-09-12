b721e84a8b6e9723c1bb17ad5e33e5e0776e1d9c
nvn41091 <nvn41091@gmail.com>
2020-09-13 00:11:25 +0700

    cm

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const app = express();
 
-app.use(express.static(__dirname + './dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
