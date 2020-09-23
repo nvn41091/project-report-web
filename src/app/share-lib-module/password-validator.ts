2ce33f246bfb56b0969e79fc49efd8e377dfc86a
nvn41091 <nvn41091@gmail.com>
2020-09-23 17:16:26 +0700

    cm

--- src/app/share-lib-module/password-validator.ts
+++ src/app/share-lib-module/password-validator.ts
@@ -2,7 +2,7 @@ import {FormGroup} from '@angular/forms';
 
 export function passwordsMatchValidator(form: FormGroup) {
   if (form.get('passwordHash') && form.get('rePassword')) {
-    if (form.get('passwordHash').value === form.get('rePassword').value) {
+    if (form.get('rePassword').value && form.get('passwordHash').value === form.get('rePassword').value) {
       form.get('rePassword').setErrors(null);
       return null;
     }
