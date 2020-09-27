36426061bec3958827378bba51e57ac52e8505d0
nvn41091 <nvn41091@gmail.com>
2020-09-27 12:01:14 +0700

    cm

--- src/app/auth/auth-expired.interceptor.ts
+++ src/app/auth/auth-expired.interceptor.ts
@@ -1,22 +1,29 @@
-import { Injectable } from '@angular/core';
+import {Inject, Injectable, Injector} from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import {NbTokenService} from '@nebular/auth';
+import {CustomToastrService} from '../shared/services/custom-toastr.service';
 
 @Injectable()
 export class AuthExpiredInterceptor implements HttpInterceptor {
   constructor(
     private router: Router,
     private jwtService: NbTokenService,
+    @Inject(Injector) private injector: Injector,
   ) {}
+
+  private get toastr(): CustomToastrService {
+    return this.injector.get(CustomToastrService);
+  }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       tap(null, (err: HttpErrorResponse) => {
         if (err.status === 401 && err.url && !err.url.includes('/authenticate')) {
           this.jwtService.clear();
+          this.toastr.error('common.label.token_expired ', true);
           this.router.navigate(['auth/login']);
         }
       }),
