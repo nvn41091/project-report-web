af2cf3ae443e9dcd271cac65d021b9b5f810c5f9
nvn41091 <nvn41091@gmail.com>
2020-10-07 08:11:15 +0700

    hi

--- src/app/shared/components/kpi-map/kpi-map.component.ts
+++ src/app/shared/components/kpi-map/kpi-map.component.ts
@@ -4,12 +4,12 @@ import {NbDialogRef} from '@nebular/theme';
 @Component({
   selector: 'ngx-kpi-map',
   templateUrl: './kpi-map.component.html',
-  styleUrls: ['./kpi-map.component.scss']
+  styleUrls: ['./kpi-map.component.scss'],
 })
 export class KpiMapComponent implements OnInit {
   items: any;
-  @Input() row: string
-  @Input() column: string
+  @Input() row: string;
+  @Input() column: string;
   constructor(public ref: NbDialogRef<KpiMapComponent>) { }
 
   ngOnInit(): void {
