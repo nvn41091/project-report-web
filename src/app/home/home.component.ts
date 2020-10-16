f1d78167a57a3e7bfae3e37015197dc238e803b3
nvn41091 <nvn41091@gmail.com>
2020-10-16 14:15:07 +0700

    cm

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,5 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {Router, NavigationStart, NavigationCancel, NavigationEnd} from '@angular/router';
-import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
-import {filter} from 'rxjs/operators';
+import {Component, ViewEncapsulation} from '@angular/core';
 
-declare let $: any;
 
 @Component({
   selector: 'ngx-home',
@@ -12,42 +8,7 @@ declare let $: any;
     <app-preloader></app-preloader>
     <router-outlet></router-outlet>
   `,
-  providers: [
-    Location, {
-      provide: LocationStrategy,
-      useClass: PathLocationStrategy
-    }
-  ],
   encapsulation: ViewEncapsulation.None
 })
-export class HomeComponent implements OnInit {
-  location: any;
-  routerSubscription: any;
-
-  constructor(private router: Router) {
-  }
-
-  ngOnInit() {
-    this.recallJsFuntions();
-  }
-
-  recallJsFuntions() {
-    this.router.events
-      .subscribe((event) => {
-        if (event instanceof NavigationStart) {
-          $('#preloader').fadeIn('slow');
-        }
-      });
-    this.routerSubscription = this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd || event instanceof NavigationCancel))
-      .subscribe(event => {
-        $.getScript('assets/js/main.js');
-        $('#preloader').fadeOut('slow');
-        this.location = this.router.url;
-        if (!(event instanceof NavigationEnd)) {
-          return;
-        }
-        window.scrollTo(0, 0);
-      });
-  }
+export class HomeComponent {
 }
