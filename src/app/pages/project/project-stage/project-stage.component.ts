0724723ef58082a11419c23a638f99ce43243854
Nguyễn Văn Ngọc <nvn41091@gmail.com>
2020-11-09 11:08:38 +0700

    cm

--- src/app/pages/project/project-stage/project-stage.component.ts
+++ src/app/pages/project/project-stage/project-stage.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  ComplexHierarchicalTree,
+  ConnectorModel,
+  DataBinding,
+  Diagram,
+  DiagramTools,
+  HierarchicalTree,
+  NodeModel,
+  SnapConstraints,
+  SnapSettingsModel
+} from '@syncfusion/ej2-diagrams';
+import {DiagramComponent} from '@syncfusion/ej2-angular-diagrams';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {DataManager} from '@syncfusion/ej2-data';
 
 @Component({
   selector: 'ngx-project-stage',
@@ -6,10 +20,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-stage.component.scss']
 })
 export class ProjectStageComponent implements OnInit {
+  loading: boolean = false;
+  @ViewChild('diagram')
+  public diagram: DiagramComponent;
+  public layout: Object = {
+    type: 'ComplexHierarchicalTree',
+    orientation: 'LeftToRight',
+    verticalSpacing: 100,
+    horizontalSpacing: 70,
+    margin: {left: 30, right: 10, top: 30, bottom: 50},
+    getLayoutInfo: (node) => {
+      if (true) {
+        node.style.fill = '#8d137e';
+      }
+    }
+  };
+  public tool: DiagramTools = DiagramTools.ZoomPan;
+  public snapSettings: SnapSettingsModel = {constraints: SnapConstraints.None};
 
-  constructor() { }
+  color = {
+    red: '#C62539',
+    gray: '#82858A',
+    green: '#4EA548',
+  };
+  dataReport: any = [{
+    icon: 'folder',
+    text: 'Test'
+  }];
+  public fields = {
+    iconCss: 'icon', tooltip: 'name'
+  };
+
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
   }
 
+  onSelectReport(data) {
+  }
+
+  loadData() {
+  }
 }
--- src/app/pages/project/project.module.ts
+++ src/app/pages/project/project.module.ts
@@ -20,24 +20,26 @@ import {SharedModule} from '../../shared/shared.module';
 import {NgxTrimDirectiveModule} from 'ngx-trim-directive';
 import { ProjectInformationUpdateComponent } from './project-information/project-information-update/project-information-update.component';
 import { ProjectStageComponent } from './project-stage/project-stage.component';
+import {ListViewModule} from "@syncfusion/ej2-angular-lists";
 
 @NgModule({
   declarations: [ProjectComponent, ProjectInformationComponent, ProjectInformationUpdateComponent, ProjectStageComponent],
-  imports: [
-    CommonModule,
-    ProjectRoutingModule,
-    NbCardModule,
-    TranslateModule,
-    NbButtonModule,
-    NbIconModule,
-    NbSecurityModule,
-    ReactiveFormsModule,
-    NbSelectModule,
-    NgxDatatableModule,
-    SharedModule,
-    NbSpinnerModule,
-    NbInputModule,
-    NgxTrimDirectiveModule,
-  ],
+    imports: [
+        CommonModule,
+        ProjectRoutingModule,
+        NbCardModule,
+        TranslateModule,
+        NbButtonModule,
+        NbIconModule,
+        NbSecurityModule,
+        ReactiveFormsModule,
+        NbSelectModule,
+        NgxDatatableModule,
+        SharedModule,
+        NbSpinnerModule,
+        NbInputModule,
+        NgxTrimDirectiveModule,
+        ListViewModule,
+    ],
 })
 export class ProjectModule { }
